84b64bf341682e300cd2144009c0716598423d5a
wjosephmark <wjosephmark@gmail.com>
2020-04-06 11:10:23 -0600

    Added route

--- app.py
+++ app.py
@@ -8,6 +8,41 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean)
+
+    def __init__(self, title, done):
+        self.title = title
+        self.done = done
+
+class TodoSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "title", "done")
+
+todo_schema = TodoSchema()
+todos_schema = TodoSchema(many=True)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1>ToDo Flask API</h1>"
+
+#GET
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    all_todos = Todo.query.all()
+    result = todos_schema.dump(all_todos)
+    return jsonify(result)
+
+#POST
+#PUT / PATCH
+#DELETE
+
 if __name__ == "__main__":
     app.debug = True
     app.run()             
