694bda34cc943090fabfa63a9603f505ef7cefac
wjosephmark <wjosephmark@gmail.com>
2020-04-06 10:12:35 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()             
