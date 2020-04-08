7652736d1b691c81713a34c46b613e17e3c9321e
wjosephmark <wjosephmark@gmail.com>
2020-04-08 10:14:34 -0600

    fixed flask cors import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_cors import flask_cors
+from flask_cors import CORS
 from flask_heroku import Heroku
 import os
 
